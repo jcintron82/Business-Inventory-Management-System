e94915262702e32a3d2274dd520032f8286c28f9
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2022-12-27 14:39:16 -0500

    Added form route and began working on new product submission form

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/form');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/form', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,12 +42,50 @@ app.listen(5000, () => {
   console.log('Running on Port 5000...')
 })
 
+//Middleare for browser live reload
+var livereload = require("livereload");
+var connectLiveReload = require("connect-livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+app.use(connectLiveReload());
+
 const mongoose=require('mongoose');
 mongoose.connect('mongodb+srv://dispensary:dispensarypassword@dispensary.gd0egr1.mongodb.net/?retryWrites=true&w=majority')
 
 const flower = require('./flower');
 const concentrate = require('./concentrates')
 
+
+
+
+const run = async (err) => {
+
+  try {
+const OGKush = await concentrate.create({ 
+  strainName: 'Blue Dream',
+  classification: 'Indica',
+  consistency: 'Budder',
+  cannabanoids: {
+    THC:20,
+    CBD:5
+  },
+  SKU: 001,
+  topTerpenes: ['Myrcene', 'Limonene' ],
+  price: 35,
+  stock: 5,
+  // similarTo: mongoose.SchemaTypes.ObjectId
+}
+) 
+console.log(OGKush)
+runConcentrates();}
+catch (err) {
+  console.log(err.message)
+}
+}
 
 const runConcentrates = async (err) => {
   try {
@@ -70,31 +108,6 @@ console.log(OGBudder)
   catch (err) {
     console.log(err.message);
   }
-}
-
-const run = async (err) => {
-
-  try {
-const OGKush = await concentrate.create({ 
-  strainName: 'Blue dream',
-  classification: 'Indica',
-  consistency: 'Budder',
-  cannabanoids: {
-    THC:20,
-    CBD:5
-  },
-  SKU: 001,
-  topTerpenes: ['Myrcene', 'Limonene' ],
-  price: 35,
-  stock: 5,
-  // similarTo: mongoose.SchemaTypes.ObjectId
-}
-) 
-console.log(OGKush)
-runConcentrates();}
-catch (err) {
-  console.log(err.message)
-}
 }
 run()
 
--- routes/form.js
+++ routes/form.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('form', { title: 'Form' });
 });
 
 module.exports = router;
