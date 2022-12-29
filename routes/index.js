c12dc51b403d3cdc1f38ba7a2e031c235ae914a1
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2022-12-29 11:27:23 -0500

    Data fetch code structure started. Set up skeleton function with a find query via the mongoose connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+const { db } = require('../flower');
 var router = express.Router();
+
+const Flower = require("../flower");
+const Concentrate = require("../concentrates");
+
+
+// const data = db.collection('concentrates').find()
+const dataFunc = () => {
+  const data = Concentrate.find({}, (err, cursor) => {
+    if (err) throw err;
+    cursor.forEach(dataset => console.log(dataset.strainName))
+  })
+}
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Your home page...' });
+
+  const units = [];
+  dataFunc()
+  res.render('index', { title: units[0] })
+
 });
 
 module.exports = router;
