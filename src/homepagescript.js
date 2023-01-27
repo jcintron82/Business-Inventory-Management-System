2c72279d9f86d96fadd614e6e0d5af53cbc4f0bd
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-01-27 15:59:35 -0500

    Reached an MVP for the home page and formatted all relevent files

--- src/homepagescript.js
+++ src/homepagescript.js
@@ -1,3 +1,12 @@
-document.getElementById('downarrowbtnid').addEventListener('click', () => {
-    document.getElementById('parallax-break-div').scrollIntoView({block: 'start', behavior: 'auto'});
-});
+document.getElementById("downarrowbtnid").addEventListener("click", () => {
+    document
+      .getElementById("parallax-break-div")
+      .scrollIntoView({ block: "start", behavior: "auto" });
+  });
+  
+  document.getElementById("totopsvg").addEventListener("click", () => {
+    document
+      .getElementById("categoriesaccordian")
+      .scrollIntoView({ block: "center", behavior: "auto" });
+  });
+  
