7b4a0b4b35bd519a522dd844f8f11e2cab3d2b17
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-01-27 15:29:55 -0500

    Added functionality to the down arrow button

---
+++ src/homepagescript.js
@@ -0,0 +1,3 @@
+document.getElementById('downarrowbtnid').addEventListener('click', () => {
+    document.getElementById('parallax-break-div').scrollIntoView({block: 'start', behavior: 'auto'});
+});
