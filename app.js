af35f56aecf5de9cf8ef12dc9d8ec882787b4131
jcintron82 <71289948+jcintron82@users.noreply.github.com>
2023-02-13 14:41:10 -0500

    debugging vercel

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.locals.basedir = path.join(__dirname, 'views');
 
 app.use(logger('dev'));
 app.use(express.json());
